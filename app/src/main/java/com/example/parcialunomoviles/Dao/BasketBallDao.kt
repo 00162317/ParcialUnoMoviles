0114313777ec00710475f94cf8caf652803768c9
Roberto Hernandez <hernandez.roberto014@gmail.com>
2019-06-01 18:57:06 -0600

    Mentira. Ya cree los paquetes. Se creo la entidad y el dao lo tengo a medias se podria decir!!

--- app/src/main/java/com/example/parcialunomoviles/Dao/BasketBallDao.kt
+++ app/src/main/java/com/example/parcialunomoviles/Dao/BasketBallDao.kt
@@ -2,6 +2,8 @@ package com.example.parcialunomoviles.Dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.parcialunomoviles.Entities.BasketBall
 
@@ -10,4 +12,7 @@ interface BasketBallDao{
 
     @Query("SELECT * FROM partido")
     fun getAll():LiveData<List<BasketBall>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(new:BasketBall)
 }

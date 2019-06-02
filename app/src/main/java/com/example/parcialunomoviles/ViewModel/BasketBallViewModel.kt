8f71960ed86315ad2f456ee1008ce421f10f49e6
Roberto Hernandez <hernandez.roberto014@gmail.com>
2019-06-01 20:06:04 -0600

    End viewModel

---
+++ app/src/main/java/com/example/parcialunomoviles/ViewModel/BasketBallViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.parcialunomoviles.ViewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.parcialunomoviles.BasketBallRoomDataBase
+import com.example.parcialunomoviles.Entities.BasketBall
+import com.example.parcialunomoviles.Repository.BasketBallRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class BasketBallViewModel(application: Application):AndroidViewModel(application){
+    private var repository: BasketBallRepository? = null
+    var allBKB: LiveData<List<BasketBall>>? = null
+
+    init {
+        val bkbDao=BasketBallRoomDataBase.getDatabase(application).BasketBallDao()
+
+        repository= BasketBallRepository(bkbDao)
+
+        allBKB=bkbDao.getAll()
+    }
+
+    fun insert(partido:BasketBall) = viewModelScope.launch(Dispatchers.IO){
+        repository!!.insertBasketball(partido)
+    }
+
+    //fun mostrarPartido() = repository!!.getAllBKB()
+}

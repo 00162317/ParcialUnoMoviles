c59c57fda97fdd307f3d2e8f77a9e5459834ff3b
Roberto Hernandez <hernandez.roberto014@gmail.com>
2019-06-01 19:04:28 -0600

    Se cambio un campito de la entidad, ahorita!!

--- app/src/main/java/com/example/parcialunomoviles/Entities/BasketBall.kt
+++ app/src/main/java/com/example/parcialunomoviles/Entities/BasketBall.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "partido")
 data class BasketBall(
-    @PrimaryKey @ColumnInfo(name = "id_partido") val id_partido:String,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id_partido") val id_partido:Int,
     @ColumnInfo(name = "equipo1") val equipo1:String,
     @ColumnInfo(name = "equipo2") val equipo2:String,
     @ColumnInfo(name = "puntuacion1") val puntuacion1:String,
